b58229936680d0d2c9992e354fa1a1364a2d430f
domanovkon <domanovkon@gmail.com>
2021-11-01 18:50:24 +0300

    add parametric search

--- laba1.py
+++ laba1.py
@@ -177,7 +177,91 @@ def getSimilarsByGroupLaptops(ds, dataSetFromTxt, metric, like_serial_number, di
 
 
 # Поиск рекомендаций для группы ноутбуков. Начальные параметры:
-favorites = "27,26,25"
-dislikes = "6,7"
-getSimilarsByGroupLaptops(ds, dataSetFromTxt, func, np.fromstring(favorites, dtype=int, sep=','),
-                          np.fromstring(dislikes, dtype=int, sep=','))
+# favorites = "27,26,25"
+# dislikes = "6,7"
+# getSimilarsByGroupLaptops(ds, dataSetFromTxt, func, np.fromstring(favorites, dtype=int, sep=','),
+#                           np.fromstring(dislikes, dtype=int, sep=','))
+
+
+# Параметрический поиск
+prices = ['высокая', 'низкая']
+gpu = ['встроенная', 'дискретная']
+laptop_types = ['ноутбуки', 'ультрабуки', 'геймерские']
+ram_amounts = [4, 8, 16, 32]
+cpu = ['amd', 'intel']
+
+while True:
+    gpu_type = str(input("Введите тип желаемой видеокарты (Встроенная или Дискретная): "))
+    if gpu_type.lower() in gpu:
+        break
+    print("Вы ввели некорректные данные")
+
+while True:
+    prices_category = str(input("Введите желаемую ценовую категорию (Высокая или Низкая): "))
+    if prices_category.lower() in prices:
+        break
+    print("Вы ввели некорректные данные")
+
+while True:
+    laptop_type = str(input("Выберите желаемую категорию (Ноутбуки, Ультрабуки или Геймерские): "))
+    if laptop_type.lower() in laptop_types:
+        break
+    print("Вы ввели некорректные данные")
+
+while True:
+    ram_amount = int(input("Введите желаемое количество оперативной памяти (4, 8, 16, 32): "))
+    if ram_amount in ram_amounts:
+        break
+    print("Вы ввели некорректные данные")
+
+while True:
+    cpu_type = str(input("Введите желаемый процессор (AMD, Intel): "))
+    if cpu_type.lower() in cpu:
+        break
+    print("Вы ввели некорректные данные")
+
+if cpu_type == "amd":
+    cpu_type = cpu_type.upper()
+else:
+    cpu_type = cpu_type.title()
+
+dataSetSearch = pd.read_csv('laptops.txt', delimiter='\t', encoding="utf-16-le",
+                            usecols=['Тип видеокарты', 'Цена', 'Категория', 'Количество_ОЗУ', 'Процессор', 'Ноутбук'])
+# dataSetSearch = pd.read_csv('laptops.txt', delimiter='\t', encoding="utf-16-le")
+
+gpu_search = dataSetSearch[dataSetSearch["Тип видеокарты"] == gpu_type.title()]
+prices_search = gpu_search[gpu_search["Цена"] == prices_category.title()]
+laptop_type_search = prices_search[prices_search["Категория"] == laptop_type.title()]
+ram_amounts_search = laptop_type_search[laptop_type_search["Количество_ОЗУ"] == ram_amount]
+cpu_search = ram_amounts_search[ram_amounts_search["Процессор"] == cpu_type]
+print("\n")
+
+
+def getSimilarsInSearch(ds, dataSet, metric, dfSearch):
+    r = []
+    for i in range(len(ds.values.tolist())):
+        r.append(metric(dfSearch.values.tolist()[0], ds.values.tolist()[i]))
+
+    return pd.DataFrame(list(zip(r, map(lambda e: str("   ".join(e[-1:])),
+                                        dataSet.values.tolist()))), index=np.arange(len(r)),
+                        columns=['Величина различия', 'Ноут'])
+
+
+def searchSimulariry():
+    dfSearch = pd.DataFrame({'Тип видеокарты': [gpu_type.title()],
+                             'Цена': [prices_category.title()],
+                             'Категория': [laptop_type.title()],
+                             'Количество_ОЗУ': ram_amount,
+                             'Процессор': cpu_type})
+
+    func = diff_tree
+    dataset = dataSetSearch
+    print(getSimilarsInSearch(dataset, dataSetSearch, func, dfSearch).sort_values("Величина различия"))
+
+
+if cpu_search.empty:
+    print("В наличии нет ноутбуков с такими параметрами. Возможно вас заинтересуют похожие ноутбуки: ")
+    searchSimulariry()
+else:
+    print("Результаты поиска: ")
+    print(cpu_search)
